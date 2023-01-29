25a79800b2e21291c9c6b80ceab51e906eaf3761
Dracula Coders <112293593+draculacoders@users.noreply.github.com>
2023-01-29 17:04:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ class Config:
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
     # The Telegram API things
     APP_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
+    API_HASH = os.environ.get("API_HASH", "")
